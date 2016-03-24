e99ddecf28ecaf331f7ba5b30832a4ebcc914c55
Tamal Mukherjee <tmukherj2@agl.com.au>
2016-03-24 12:20:06 +1100

    ok

---
+++ WebRole1/Controllers/AccountController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRole1.Controllers
+{
+    public class AccountController : Controller
+    {
+        //http://www.codeproject.com/Tips/849113/Four-Easy-Steps-to-Set-Up-OWIN-for-Form-authentica
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            if (User.Identity.IsAuthenticated)
+                ViewBag.Message = "You Dont have enough Permissions, you need to be with elevated privileges to go there";
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string userid, string password, string returnUrl)
+        {
+            if (userid.Equals(password)) //login check logic
+            {
+                List<Claim> claims = GetClaims();
+                if (null != claims)
+                {
+                    SignIn(claims);
+
+                    return RedirectToLocal(returnUrl);
+                }
+            }
+            return Content("error");
+        }
+
+        private List<Claim> GetClaims()
+        {
+            var claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.Email, "tmukherj2@agl.com.au"));            
+            claims.Add(new Claim(ClaimTypes.Name, "Tamal Mukherjee"));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, "A105254"));
+
+            var roles = new[] { "Admin", "Citizin", "Worker" };
+            var groups = new[] { "Admin", "Citizin", "Worker" };
+
+            foreach (var item in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, item));
+            }
+            foreach (var item in groups)
+            {
+                claims.Add(new Claim(ClaimTypes.GroupSid, item));
+            }
+            return claims;
+        }
+
+        private void SignIn(List<Claim> claims)
+        {
+            var claimsIdentity = new ClaimsIdentity(claims,    DefaultAuthenticationTypes.ApplicationCookie);
+
+            //This uses OWIN authentication
+
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ExternalCookie);
+            AuthenticationManager.SignIn(new AuthenticationProperties()
+            {
+                IsPersistent = false
+            }, claimsIdentity);
+
+            HttpContext.User = new ClaimsPrincipal(AuthenticationManager.AuthenticationResponseGrant.Principal);
+        }
+
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut();
+            return RedirectToAction("Index", "Home", new { area = "" });
+        }
+    }
+}
---
+++ WebRole1/Startup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Security.Claims;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.AspNet.Identity;
+using System.Web.Helpers;
+
+[assembly: OwinStartup(typeof(WebRole1.Startup))]
+
+namespace WebRole1
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            System.Web.Helpers.AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Email;
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+                CookieSecure = CookieSecureOption.Always,
+                SlidingExpiration = true,
+                ExpireTimeSpan = TimeSpan.FromMinutes(30)
+            });
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
+        }
+    }
+}
